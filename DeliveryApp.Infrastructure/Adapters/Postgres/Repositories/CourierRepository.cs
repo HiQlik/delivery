980efe1d16888b3f9cccd2e1d163dd36508f3b05
NNN VVV <exspectati@gmail.com>
2024-06-13 00:43:37 +0300

    test4

---
+++ DeliveryApp.Infrastructure/Adapters/Postgres/Repositories/CourierRepository.cs
@@ -0,0 +1,54 @@
+﻿using DeliveryApp.Core.Domain.CourierAggregate;
+using DeliveryApp.Core.Ports;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeliveryApp.Infrastructure.Adapters.Postgres.Repositories;
+
+public class CourierRepository : ICourierRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public CourierRepository(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+    }
+
+    public async Task<Courier> AddAsync(Courier courier)
+    {
+        if (courier.Transport != null) _dbContext.Attach(courier.Transport);
+
+        if (courier.CourierStatus != null) _dbContext.Attach(courier.CourierStatus);
+
+        var result = await _dbContext.Couriers.AddAsync(courier);
+        return result.Entity;
+    }
+
+    public void Update(Courier courier)
+    {
+        if (courier.Transport != null) _dbContext.Attach(courier.Transport);
+
+        if (courier.CourierStatus != null) _dbContext.Attach(courier.CourierStatus);
+
+        _dbContext.Couriers.Update(courier);
+    }
+
+    public async Task<Courier> GetAsync(Guid courierId)
+    {
+        var courier = await _dbContext
+            .Couriers
+            .Include(x => x.Transport)
+            .Include(x => x.CourierStatus)
+            .FirstOrDefaultAsync(o => o.Id == courierId);
+        return courier;
+    }
+
+    public IEnumerable<Courier> GetAllReady()
+    {
+        var couriers = _dbContext
+            .Couriers
+            .Include(x => x.Transport)
+            .Include(x => x.CourierStatus)
+            .Where(o => o.CourierStatus == CourierStatus.Ready);
+        return couriers;
+    }
+}
---
+++ DeliveryApp.Infrastructure/Adapters/Postgres/Repositories/OrderRepository.cs
@@ -0,0 +1,57 @@
+﻿using DeliveryApp.Core.Domain.OrderAggregate;
+using DeliveryApp.Core.Ports;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeliveryApp.Infrastructure.Adapters.Postgres.Repositories;
+
+public class OrderRepository : IOrderRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public OrderRepository(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+    }
+
+    public async Task<Order> AddAsync(Order order)
+    {
+        if (order.OrderStatus != null) _dbContext.Attach(order.OrderStatus);
+
+        var result = await _dbContext.Orders.AddAsync(order);
+        return result.Entity;
+    }
+
+    public void Update(Order order)
+    {
+        if (order.OrderStatus != null) _dbContext.Attach(order.OrderStatus);
+
+        _dbContext.Orders.Update(order);
+    }
+
+    public async Task<Order> GetAsync(Guid orderId)
+    {
+        var order = await _dbContext
+            .Orders
+            .Include(x => x.OrderStatus)
+            .SingleOrDefaultAsync(o => o.Id == orderId);
+        return order;
+    }
+
+    public IEnumerable<Order> GetAllCreated()
+    {
+        var orders = _dbContext
+            .Orders
+            .Include(x => x.OrderStatus)
+            .Where(o => o.OrderStatus == OrderStatus.Created);
+        return orders;
+    }
+
+    public IEnumerable<Order> GetAllAssigned()
+    {
+        var orders = _dbContext
+            .Orders
+            .Include(x => x.OrderStatus)
+            .Where(o => o.OrderStatus == OrderStatus.Assigned);
+        return orders;
+    }
+}
